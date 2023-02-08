9738716b9f417caf49edddcaffc753c5fd36550e
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-08 18:14:01 +0000

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,30 +5,15 @@ import requests
 from sys import argv
 if __name__ == "__main__":
     api_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(api_url, verify=False).json()
     response = requests.get("{}users/{}".format(api_url, argv[1]))
-    n = response.json().get("name")
+    n = user.get('username')
     if n is not None:
         all_tasks = requests.get(
                 "{}todos?userId={}".format(
                     api_url, argv[1])).json()
-        rows = [[]]
-        r = []
-        for dict in all_tasks:
-            for k,v in dict.items():
-                if k != "id":
-                    r.append(v)
-                if k == "userId":
-                    filename = "{}.csv".format(v)
-                    r.append(n)
-            #h = r[3]
-            #m = r[2]
-            #r[2] = h
-            #r[3] = m
-            rows.append(r)
-            r = []
-        
-        #"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-        with open(filename, 'w') as csvfile:
-            csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            csvwriter.writerows(rows)
 
+        with open("{}.csv".format(argv[1]), 'w') as csvfile:
+            csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            for i in all_tasks:
+                csvwriter.writerow("[i["userId"], n, i["completed"], i["title"])
