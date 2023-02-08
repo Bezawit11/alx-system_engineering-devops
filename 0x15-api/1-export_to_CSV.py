0e914a5b7c0ef7592240d6897d04723fa07c7382
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-08 17:20:55 +0000

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python3
+"""Gather data from an API"""
 import csv
-filename = "USER_ID.csv"
+import requests
+from sys import argv
+if __name__ == "__main__":
+    api_url = "https://jsonplaceholder.typicode.com/"
+    response = requests.get("{}users/{}".format(api_url, argv[1]))
+    n = response.json().get("name")
+    if n is not None:
+        all_tasks = requests.get(
+                "{}todos?userId={}".format(
+                    api_url, argv[1])).json()
+        rows = [[]]
+        r = []
+        for dict in all_tasks:
+            for k,v in dict.items():
+                if k != "id":
+                    r.append(v)
+                if k == "userId":
+                    filename = "{}.csv".format(v)
+            rows.append(r)
+            r = []
+        
+        #"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+        with open(filename, 'w') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerows(rows)
 
