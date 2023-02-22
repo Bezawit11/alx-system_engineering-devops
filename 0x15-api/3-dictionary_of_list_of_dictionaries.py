1c35d6c93a8660746af5bb498c4c02bac68a5b9a
Bezawit11 <socbeza13@gmail.com>
2023-02-22 09:19:21 +0000

    last task

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import json
+import requests
+from sys import argv
+if __name__ == "__main__":
+    api_url = "https://jsonplaceholder.typicode.com/"
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    dictionary = {}
+    for a in range(len(response.json())):
+        n = response.json()[a]['username']
+        all_tasks = requests.get(
+            "{}todos?userId={}".format(
+                api_url, a)).json()
+        j = []
+        for i in all_tasks:
+            u = i['userId']
+            k = {'username': n, 'title': i['title'], 'completed': i['completed']}
+            j.append(k)
+        c = a + 1
+        dictionary = {c: j}
+        j = []
+    filename = "todo_all_employees.json"
+    with open(filename, 'w', newline='') as jsonfile:
+        json.dump(dictionary, jsonfile)
