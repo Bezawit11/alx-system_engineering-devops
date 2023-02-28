3749c76c07566cf653784aaef21d29c3bf53a0f9
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-28 20:12:20 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """returns number of subscribers from reddit api"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/Jazzlike_Day2550)'})
+v1.0.0 (by /u/Jazzlike_Day2550)'}, allow_redirects=False)
     js = response.json()
     subs = js.get("data", {}).get("subscribers", 0)
     return subs
