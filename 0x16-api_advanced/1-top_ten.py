5af9c9d350cd676a8f68fb863fda93e52fae63c8
Bezawit11 <socbeza13@gmail.com>
2023-02-28 19:46:50 +0000

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,6 +10,8 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     response = requests.get(url, headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/Jazzlike_Day2550)'})
+    if response.status_code != 200:
+        print("None")
     js = response.json()
     l = js.get("data")
     for i in l.get("children"):
