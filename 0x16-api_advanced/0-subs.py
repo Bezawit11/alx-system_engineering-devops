67697a0c1023999314e3795dfbdf9ea46633d212
Bezawit11 <socbeza13@gmail.com>
2023-02-28 11:38:21 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,6 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/Jazzlike_Day2550)'})
     if response is None:
-        print("0")
         return 0
     js = response.json()
     subs = js.get("data").get("subscribers")
