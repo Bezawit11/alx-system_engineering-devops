ef5332e53ed907660e23d676925be9f7e022e946
Bezawit11 <socbeza13@gmail.com>
2023-02-28 18:33:07 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,8 +3,6 @@
 import requests
 import json
 def number_of_subscribers(subreddit):
-    if type(subreddit) is not str:
-        return 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/Jazzlike_Day2550)'})
