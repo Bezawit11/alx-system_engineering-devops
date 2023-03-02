6328714659fc7ebd416489bd282699d93fd24d9a
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-03-02 13:50:35 +0000

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,9 +11,7 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     response = requests.get(url, headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/Jazzlike_Day2550)'}, allow_redirects=False)
-    if response.status_code != 200:
-        print("None")
-    else:
+    if response.status_code == 200:
         js = response.json()
         l = js.get("data")
         for i in l.get("children"):
@@ -21,3 +19,5 @@ v1.0.0 (by /u/Jazzlike_Day2550)'}, allow_redirects=False)
                 print(i.get("data").get("title"))
             except:
                 print(i.get("data").get("title").encode())
+    else:
+        print("None")
