69d387a4970ae42234a98e1df09eb9d7ca03bec3
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-03-02 19:39:19 +0000

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # puppet file to fix  500 internal server error
 exec { 'fixed_false':
-  command => "sed -i 's/define('WP_DEBUG', false);/define('WP_DEBUG', true);/' /var/www/html/wp-config.php",
+  command => "sed -i 's/('WP_DEBUG', false);/('WP_DEBUG', true);/' /var/www/html/wp-config.php",
   path    => '/bin';
 }
