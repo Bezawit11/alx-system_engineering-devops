959f65beabe19163deb34dfaa00be6e168067e19
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-03-04 08:14:39 +0000

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,11 +5,12 @@ returns a list containing the titles of all hot articles
 import requests
 import json
 
-def recurse(subreddit, hot_list=[], after=""):
+def recurse(subreddit, hot_list=[], after=None):
     """ """
     url = 'https://www.reddit.com/r/{}/hot.json?'.format(subreddit)
+    parameter = {'after': after}
     response = requests.get(url, headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/Jazzlike_Day2550)'}, params=after, allow_redirects=False)
+v1.0.0 (by /u/Jazzlike_Day2550)'}, params=parameter, allow_redirects=False)
     if response.status_code != 200:
         return None
     js = response.json()
@@ -18,7 +19,7 @@ v1.0.0 (by /u/Jazzlike_Day2550)'}, params=after, allow_redirects=False)
     for i in l.get("children"):
         title = i.get("data").get("title")
         hot_list.append(title)
-    if after:
+    if after is not None:
         recurse(subreddit, hot_list, after)
     else:    
         return hot_list
