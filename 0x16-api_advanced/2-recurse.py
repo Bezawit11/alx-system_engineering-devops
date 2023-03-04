c1ef6b32034d37db9c861a972cde1587e80fea6f
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-03-04 08:24:17 +0000

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,6 +5,7 @@ returns a list containing the titles of all hot articles
 import json
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
     """ recursive function that queries the Reddit API and returns a 
     list containing the titles of all hot articles for a given subreddit
